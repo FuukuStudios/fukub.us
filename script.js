e2950961fb89069bb282ed16155951710df4b407
unrvldswtr <37597167+CheezyAmuzus@users.noreply.github.com>
2024-03-01 03:49:51 -0800

    A lot of new changes! Including new sections!
    Added javascript to make a cute note when scrolling down.
    Also, added cool background gradient for the sections
    Fixed up fonts and cleaned up a little more of the code

---
+++ script.js
@@ -0,0 +1,2 @@
+"use strict";console.log("Welcome to Hell!");var scrollNotice=document.getElementById("scrollNotice");window.addEventListener("scroll",function(){550<=window.scrollY&&(scrollNotice.innerHTML='<i class="fa-solid fa-caret-down"></i>Thanks <3<i class="fa-solid fa-caret-down"></i>')});
+//# sourceMappingURL=script.js.map
