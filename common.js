95013125704196c4df3dac874ce7d23a097df25a
unrvldswtr <37597167+CheezyAmuzus@users.noreply.github.com>
2024-03-07 01:18:50 -0800

    Complete overhaul
    Optimized and added fonts
    Layout change
    Removed the webp shit
    Split up js files

---
+++ common.js
@@ -0,0 +1,2 @@
+"use strict";$(window).on("load",function(){$(".loader").delay(600).fadeOut(300)}),$(document).ready(function(){$.fancybox.defaults.infobar=!1,$.fancybox.defaults.arrows=!1,$.fancybox.defaults.touch=!1,$.fancybox.defaults.hash=!1});
+//# sourceMappingURL=common.js.map
--- script.js
+++
@@ -1,2 +0,0 @@
-"use strict";console.log("Welcome to Hell!");var scrollNotice=document.getElementById("scrollNotice");window.addEventListener("scroll",function(){550<=window.scrollY&&(scrollNotice.innerHTML='<i class="fa-solid fa-caret-down"></i>Thanks <3<i class="fa-solid fa-caret-down"></i>')});
-//# sourceMappingURL=script.js.map
