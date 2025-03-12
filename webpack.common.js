1925712f10c80ad64990ea5c7b967dd08efed0af
SoniaNvm <thyrafukubus@gmail.com>
2025-03-12 00:52:25 -0700

    v1.3 - Added more details finally
    Added description and featured image. Changed to yarn berry from npm. Removed unnecessary files from /img/

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,23 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    app: './js/app.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    filename: './js/app.js',
-  },
+	entry: {
+		app: './js/app.js',
+	},
+	output: {
+		filename: './js/app.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.js', '.json'],
+	},
 };
